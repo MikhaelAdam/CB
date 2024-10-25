e4ec53c17fb583b5bc7546173709bf2af90f0e2b
MikhaelAdam <mikhaeladams737@gmail.com>
2024-10-25 09:09:37 +0700

    Sprites

--- scene/entity/anakDajjal/anak_dajjal.gd
+++ scene/entity/anakDajjal/anak_dajjal.gd
@@ -1,22 +1,9 @@
 extends CharacterBody2D
 
+var speed = 400  # speed in pixels/sec
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-
-func _physics_process(delta: float) -> void:
-
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down").normalized()
+	velocity = direction * speed
 
 	move_and_slide()
